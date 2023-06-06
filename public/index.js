0834fd9f6ebe066efcad83f4ccf37498e5fa8c21
aminatol <aminatoluwo@gmail.com>
2023-06-06 16:39:05 -0400

    created updateCounter funtion

--- public/index.js
+++ public/index.js
@@ -1,26 +1,38 @@
-async function main(){
-    const countContainer = document.querySelector('#count-container');
-    const incrementButton = document.querySelector('#increment-button');
-    const decrementButton = document.querySelector('#decrement-button');
+async function main() {
+  const countContainer = document.querySelector("#count-container");
+  const incrementButton = document.querySelector("#increment-button");
+  const decrementButton = document.querySelector("#decrement-button");
+  const url = "http://localhost:9001/counter";
 
-    const response = await fetch('http://localhost:9001/counter');
+  const response = await fetch(url);
 
-    const result = await response.json();
-    
-    let countValue = result.value;
+  const result = await response.json();
 
-    function increment(){
-        countValue++;
-        countContainer.textContent = countValue;
-    }
+  let countValue = result.value;
 
-    function decrement(){
-        countValue--;
-        countContainer.textContent = countValue;
-    }
-
-    incrementButton.addEventListener('click', increment);
-    decrementButton.addEventListener('click', decrement);
+  function increment() {
+    countValue++;
     countContainer.textContent = countValue;
+    updateCounter(countValue);
+  }
+
+  function decrement() {
+    countValue--;
+    countContainer.textContent = countValue;
+    updateCounter(countValue);
+  }
+
+  function updateCounter(countValue) {
+    fetch(url, {
+      method: "PATCH",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        value: countValue,
+      }),
+    });
+  }
+  incrementButton.addEventListener("click", increment);
+  decrementButton.addEventListener("click", decrement);
+  countContainer.textContent = countValue;
 }
-main()
+main();
